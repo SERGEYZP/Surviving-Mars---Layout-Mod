13fb92a57d44d9741ef2553c642d14c4f9d5890b
kyklish <kyklish@gmail.com>
2020-12-07 21:06:01 +0200

    v1.0.3

--- metadata.lua
+++ metadata.lua
@@ -24,7 +24,7 @@ return PlaceObj('ModDef', {
 	'author', "Kyklish",
 	'version_major', 1,
 	'version_minor', 0,
-	'version', 2,
+	'version', 3,
 	'lua_revision', 233360,
 	'saved_with_revision', 249143,
 	'code', {
